f6f92dcb3993a67f37284cff6e530b1f85f6892e
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-29 16:09:29 +0000

    Feature: Holberton.h. betty fixed

--- holberton.h
+++ holberton.h
@@ -5,19 +5,24 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
-
+/**
+ * struct print_data - Struct print_data
+ *
+ * @form: form format to check
+ * @print: pointer to function to print
+ */
 typedef struct print_data
 {
-   const char form;
-   int (*print)(va_list *lalista, int*);
-}print_data_t;
+	onst char form;
+	int (*print)(va_list *lalista, int*);
+} print_data_t;
 
-int _printf(const char * format, ...);
+int _printf(const char *format, ...);
 int _putchar(char c);
 /* task 0 */
-int print_char (va_list *lalista, int *tot);
-int print_str (va_list *lalista,int *tot);
-int print_percent (int *tot);
+int print_char(va_list *lalista, int *tot);
+int print_str(va_list *lalista, int *tot);
+int print_percent(int *tot);
 
 
 #endif /* HOLBERTON_H */
