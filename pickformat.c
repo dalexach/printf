2cc8dc7d285e9762c19da5e86383a167c6ed5d39
DNeosphere <daniela.gomezp05@outlook.com>
2019-07-30 00:11:06 +0000

    Feature: Function with a switch to compare the cases with the format given to print it according to that

---
+++ pickformat.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+/**
+ * pickformat - compares the format given with a bunch of cases to print
+ * @lalista: list of parameters to print
+ * @format: input given
+ * Return: amount of printed bytes
+ *
+ **/
+int pickformat(va_list lalista, const char *format)
+{
+	int i, lenprinted = 0;
+	char c;
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (!format[i])
+				return (-1);
+			switch (format[i + 1])
+			{
+				case 'c':
+					c = va_arg(lalista, int);
+					_putchar(c);
+					i++;
+					break;
+				case 's':
+					lenprinted = print_str(lalista);
+					lenprinted--;
+					i++;
+					break;
+				case '%':
+					_putchar('%');
+					i++;
+					break;
+				 case '\0':
+					lenprinted = -2;
+					break;
+				default:
+					_putchar(%);
+					break;
+			}
+		}
+			else
+				_putchar(format[i]);
+				lenprinted++;
+	}
+	return (lenprinted);
+}
